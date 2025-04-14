0f2275e1aa725b28b70071d52842510043405bb1
Arkadiuszglab <arek.glab97@gmail.com>
2025-04-14 20:33:07 +0200

    Added main component.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,138 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CampaignsService } from './services/campaigns.service';
+import { of } from 'rxjs';
+import { Campaign } from './models/campaign.model';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let mockCampaignService: jasmine.SpyObj<CampaignsService>;
+
+  const fakeCampaigns: Campaign[] = [
+    {
+      id: 'str123',
+      name: 'camp 13000',
+      keywords: [
+        'discount',
+        'sale',
+        'new arrival',
+        'limited edition',
+        'exclusive',
+      ],
+      bidAmount: 500000,
+      campaignFund: 5000000,
+      status: false,
+      town: 'Warsaw',
+      radius: 20,
+    },
+    {
+      id: 'str125',
+      name: 'camp 3',
+      keywords: ['clothes', 'fashion', 'design'],
+      bidAmount: 2500,
+      campaignFund: 500,
+      status: true,
+      town: 'Łódź',
+      radius: 20,
+    },
+  ];
+
   beforeEach(async () => {
+    mockCampaignService = jasmine.createSpyObj('CampaignsService', [
+      'getCampaigns',
+      'createCampaign',
+      'updateCampaign',
+      'deleteCampaign',
+    ]);
+
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [{ provide: CampaignsService, useValue: mockCampaignService }],
     }).compileComponents();
-  });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
 
-  it(`should have the 'futurum-rekrutacja' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('futurum-rekrutacja');
-  });
+    mockCampaignService.getCampaigns.and.returnValue(of(fakeCampaigns));
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, futurum-rekrutacja');
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call loadCampaigns on init', () => {
+    mockCampaignService.getCampaigns.and.returnValue(of(fakeCampaigns));
+
+    component.ngOnInit();
+
+    expect(mockCampaignService.getCampaigns).toHaveBeenCalled();
+    expect(component.campaigns.length).toBe(2);
+  });
+
+  it('should call handleEditCampaign if campaign exists', () => {
+    const updated = {
+      id: 'str123',
+      name: 'Updated Campaign',
+      bidAmount: 600000,
+    };
+    component.campaigns = fakeCampaigns;
+
+    mockCampaignService.updateCampaign.and.returnValue(of({}));
+    mockCampaignService.getCampaigns.and.returnValue(of(fakeCampaigns));
+
+    component.handleAddOrEditCampaign(updated);
+
+    expect(mockCampaignService.updateCampaign).toHaveBeenCalledWith(
+      'str123',
+      updated
+    );
+    expect(mockCampaignService.getCampaigns).toHaveBeenCalled();
+  });
+
+  it('should return "Edit Campaign" if currentCampaign has id', () => {
+    component.currentCampaign = fakeCampaigns[0];
+    expect(component.getModalTitle()).toBe('Edit Campaign');
+  });
+
+  it('should return "Add Campaign" if no currentCampaign or no id', () => {
+    component.currentCampaign = undefined;
+    expect(component.getModalTitle()).toBe('Add Campaign');
+  });
+
+  it('should call createCampaign and reload campaigns', () => {
+    const newCampaign = fakeCampaigns[0];
+    mockCampaignService.createCampaign.and.returnValue(of({}));
+    mockCampaignService.getCampaigns.and.returnValue(of(fakeCampaigns));
+
+    component.handleAddCampaign(newCampaign);
+
+    expect(mockCampaignService.createCampaign).toHaveBeenCalledWith(
+      newCampaign
+    );
+    expect(mockCampaignService.getCampaigns).toHaveBeenCalled();
+  });
+
+  it('should delete campaign and reload', () => {
+    component.campaigns = fakeCampaigns;
+
+    mockCampaignService.deleteCampaign.and.returnValue(of(void 0));
+    mockCampaignService.getCampaigns.and.returnValue(of([]));
+
+    component.handleDeleteCampaign('str123');
+
+    expect(mockCampaignService.getCampaigns).toHaveBeenCalled();
+  });
+
+  it('should call handleAddCampaign if campaign does not exist', () => {
+    const newCampaign = { ...fakeCampaigns[0], id: '123' };
+    component.campaigns = fakeCampaigns;
+    spyOn(component, 'handleAddCampaign');
+
+    component.handleAddOrEditCampaign(newCampaign);
+
+    expect(component.handleAddCampaign).toHaveBeenCalledWith(newCampaign);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,84 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SimpleModalComponent } from './components/simple-modal/simple-modal.component';
+import { CampaignListComponent } from './components/campaign-list/campaign-list.component';
+import { CampaignFormComponent } from './components/campaign-form/campaign-form.component';
+import { CommonModule } from '@angular/common';
+import { Campaign } from './models/campaign.model';
+import { CampaignsService } from './services/campaigns.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SimpleModalComponent,
+    CampaignListComponent,
+    CampaignFormComponent,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'futurum-rekrutacja';
+  isModalOpen = false;
+  campaigns: Campaign[] = [];
+  currentCampaign: Campaign | undefined = undefined;
+
+  constructor(private campaignService: CampaignsService) {}
+
+  ngOnInit(): void {
+    this.loadCampaigns();
+  }
+
+  loadCampaigns(): void {
+    this.campaignService.getCampaigns().subscribe((data) => {
+      this.campaigns = data;
+    });
+  }
+
+  getModalTitle() {
+    if (this.currentCampaign?.id) {
+      return 'Edit Campaign';
+    } else {
+      return 'Add Campaign';
+    }
+  }
+
+  handleAddCampaign(newCampaign: Campaign) {
+    this.campaignService
+      .createCampaign(newCampaign)
+      .subscribe(() => this.loadCampaigns());
+  }
+
+  handleEditCampaign(edittedCampaignId: string, newCampaign: Campaign) {
+    this.campaignService
+      .updateCampaign(edittedCampaignId, newCampaign)
+      .subscribe(() => this.loadCampaigns());
+  }
+
+  handleAddOrEditCampaign(newCampaign: Campaign) {
+    const edittedCampaign = this.campaigns.find(
+      (campaing) => campaing.id === newCampaign.id
+    );
+
+    if (edittedCampaign?.id) {
+      this.handleEditCampaign(edittedCampaign.id, newCampaign);
+    } else {
+      this.handleAddCampaign(newCampaign);
+    }
+  }
+
+  handleDeleteCampaign(id: string) {
+    this.campaignService
+      .deleteCampaign(id)
+      .subscribe(() => this.loadCampaigns());
+  }
+
+  openEditModal(id: string) {
+    this.isModalOpen = true;
+    this.currentCampaign = this.campaigns.find(
+      (campaign) => campaign.id === id
+    );
+  }
 }
